826cc6a7e77432b4da73c0c7a77cafa36300b2e2
Steve Fallet <steve.fallet@divtec.ch>
2026-04-23 10:59:47 +0200

    feat: enhance PokemonCard component with error handling for image loading

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -4,11 +4,20 @@
     hover
   >
     <v-img
-      :src="getImageUrl(pokemon.img)"
+      :src="getImageUrl(pokemon.img + 'toto')"
       :alt="pokemon.name"
       height="200"
       cover
-    />
+    >
+      <template #error>
+        <v-img
+          :src="getImageUrl('pikachu.png')"
+          alt="Erreur de chargement"
+          height="200"
+          cover
+        />
+      </template>
+    </v-img>
     <v-card-title>{{ pokemon.name }}</v-card-title>
     <v-card-subtitle>Niveau {{ pokemon.level }}</v-card-subtitle>
   </v-card>
